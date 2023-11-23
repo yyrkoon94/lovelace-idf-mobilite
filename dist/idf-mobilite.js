64a7f18d1383bad091b8f8409015c61b35450367
yyrkoon94 <dmassou@hotmail.com>
2023-11-23 08:11:37 +0000

    fix issue and line test

--- dist/idf-mobilite.js
+++ dist/idf-mobilite.js
@@ -45,7 +45,7 @@ const sncfLineColor = {
 
 class IDFMobiliteCard extends LitElement {
     static get properties() {
-        console.log("%c Lovelace - IDF Mobilité  %c 0.1.10", "color: #FFFFFF; background: #5D0878; font-weight: 700;", "color: #fdd835; background: #212121; font-weight: 700;")
+        console.log("%c Lovelace - IDF Mobilité  %c 0.1.11", "color: #FFFFFF; background: #5D0878; font-weight: 700;", "color: #fdd835; background: #212121; font-weight: 700;")
         return {
             hass: {},
             config: {},
